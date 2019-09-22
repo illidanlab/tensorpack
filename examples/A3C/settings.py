3d4b365ec3b7454d7aabaf997bb92e07882979d7
ZhuangdiZhu <zhuangdizhu@yahoo.com>
2019-09-22 13:09:22 -0400

    force adding settings.py

---
+++ examples/A3C/settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+path_prefix = "/mnt/research/judy/reward_shaping" 
+expert_data_path = {
+    "Pong-v0": "{}/expert_data/Pong-v0".format(path_prefix),
+    "Gravitar-v0": "{}/expert_data/Gravitar-v0".format(path_prefix)
+}
+
+pretraind_model_path = {
+    "Pong-v0": "{}/pretrained_models/Pong-v0.npz".format(path_prefix),
+    "Gravitar-v0": "{}/pretrained_models/Gravitar-v0.tfmodel".format(path_prefix)
+}
+
+supervised_model_checkpoint = {
+    "Pong-v0": "{}/sanity/model_checkpoint/".format(path_prefix),
+    "Gravitar-v0": "{}/sanity/Gravitar-v0/supervised_model_checkpoint".format(path_prefix)
+}
